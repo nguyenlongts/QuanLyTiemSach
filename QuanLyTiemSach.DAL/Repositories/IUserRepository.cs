1c1bdd51e7d051898fc3b9dafd59c79c826dfa5e
Long <Long@ADMIN-PC>
2025-12-26 14:39:35 +0700

    thêm mới

---
+++ QuanLyTiemSach.DAL/Repositories/IUserRepository.cs
@@ -0,0 +1,27 @@
+﻿
+using QuanLyTiemSach.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyTiemSach.DAL.Repositories
+{
+
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAllAsync();
+        Task<User?> GetByIdAsync(int id);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+        Task<List<User>> SearchAsync(string keyword);
+        Task<User?> LoginAsync(string username, string password);
+
+        Task<User?> GetByUsernameAsync(string username);
+        Task<User?> GetByEmailAsync(string email);
+
+    }
+
+}
---
+++ QuanLyTiemSach.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuanLyTiemSach.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyTiemSach.DAL.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly BookStoreDbContext _context;
+
+        public UserRepository(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User?> GetByIdAsync(int id)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(x => x.UserID == id);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(User user)
+        {
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<User>> SearchAsync(string keyword)
+        {
+            return await _context.Users
+                .Where(x =>
+                    x.Username.Contains(keyword) ||
+                    x.FullName.Contains(keyword) ||
+                    x.Email.Contains(keyword))
+                .ToListAsync();
+        }
+
+        public async Task<User?> LoginAsync(string username, string password)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u =>
+                u.Username == username &&
+                u.Password == password &&
+                u.Status == "Active");
+        }
+
+
+        public async Task<User?> GetByUsernameAsync(string username)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(x => x.Username == username);
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(x => x.Email == email);
+        }
+
+    }
+}
